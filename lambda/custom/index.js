329a7f6b375ba8bea9b5dba9e214524e909a1e95
Sade <smithsade13@gmail.com>
2018-08-23 08:30:37 -0500

    figuring out persistnace adaptors

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,14 +1,68 @@
 /* eslint-disable  func-names */
 /* eslint-disable  no-console */
-
+//TODO Add auto-scaling for Dynamo
+//TODO register for aws credits
 const Alexa = require('ask-sdk-core');
+const { DynamoDbPersistenceAdapter } = require('ask-sdk-dynamodb-persistence-adapter');
+const dynamoDbPersistenceAdapter = new DynamoDbPersistenceAdapter({ tableName : 'disaster_ready_session_data', createTable: true });
 
+//Example of persistence attributes
+//https://github.com/alexa/skill-sample-nodejs-highlowgame/blob/master/lambda/custom/index.js
+//Perhaps use these methods attached to this for the save and retrieve: dynamoDbPersistenceAdapter
+
+// const PersistentAttributesHandler = {
+//   canHandle(handlerInput) {
+//     return new Promise((resolve, reject) => {
+//       handlerInput.attributesManager.getPersistentAttributes()
+//         .then((attributes) => {
+//           resolve(attributes.foo === 'bar');
+//         })
+//         .catch((error) => {
+//           reject(error);
+//         })
+//     });
+//   },
+//   handle(handlerInput) {
+//     return new Promise((resolve, reject) => {
+//       handlerInput.attributesManager.getPersistentAttributes()
+//         .then((attributes) => {
+//           attributes.foo = 'bar';
+//           handlerInput.attributesManager.setPersistentAttributes(attributes);
+
+//           return handlerInput.attributesManager.savePersistentAttributes();
+//         })
+//         .then(() => {
+//           resolve(handlerInput.responseBuilder
+//             .speak('Persistent attributes updated!')
+//             .getResponse());
+//         })
+//         .catch((error) => {
+//           reject(error);
+//         });
+//     });
+//   },
+// };
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
   handle(handlerInput) {
+    const attributesManager = handlerInput.attributesManager;
+    const sessionAttributes = attributesManager.setPersistentAttributes(handlerInput.requestEnvelope.session);
+    const sessionAttributes2 = attributesManager.getPersistentAttributes();
+    const userId  = handlerInput.requestEnvelope.session.user;
+    console.log('atts', userId);
+    console.log('session', sessionAttributes2);
     const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+
+    // const attributes = await attributesManager.getPersistentAttributes() || {};
+    // if (Object.keys(attributes).length === 0) {
+    //   attributes.endedSessionCount = 0;
+    //   attributes.gamesPlayed = 0;
+    //   attributes.gameState = 'ENDED';
+    // }
+
+    // attributesManager.setSessionAttributes(attributes);
 
     return handlerInput.responseBuilder
       .speak(speechText)
@@ -100,5 +154,6 @@ exports.handler = skillBuilder
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
   )
+  .withPersistenceAdapter(dynamoDbPersistenceAdapter)
   .addErrorHandlers(ErrorHandler)
   .lambda();
